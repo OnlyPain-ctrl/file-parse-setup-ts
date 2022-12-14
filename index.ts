e2a4811884d1f9b182feb40b94c2f226d7aea43b
M. Gschwandtner <84477901+OnlyPain-ctrl@users.noreply.github.com>
2022-12-14 01:07:19 +0100

    added time, small changes

--- index.ts
+++ index.ts
@@ -1,29 +1,26 @@
 import { parse } from './data/parse'
 import fs from 'fs'
 
-init()
-
-async function init() {
-    const input = await getFile()
-    writeOut(await parse(input))
-}
+getFile().then((i) => parse(i).then((o) => writeOut(o)))
 
 async function getFile() {
+    console.time('time')
+
     return fs.readFileSync('./data/input.txt', 'utf8')
 }
 
 async function writeOut(data: string) {
-    const timestamp = Math.floor(Date.now() / 1000)
-
     if (!fs.existsSync('./data')) fs.mkdirSync('./data')
 
     if (fs.existsSync('./data/output.txt')) {
         if (!fs.existsSync('./data/old')) fs.mkdirSync('./data/old')
         fs.renameSync(
             './data/output.txt',
-            './data/old/output_' + timestamp + '.txt'
+            './data/old/output_' + Math.floor(Date.now() / 1000) + '.txt'
         )
     }
 
     fs.writeFileSync('./data/output.txt', data)
+
+    console.timeEnd('time')
 }
